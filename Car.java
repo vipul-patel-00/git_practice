fa517585f54d2eba75e4e3e06205fbd3adda930f
vipul <vipul100vp@gmail.com>
2024-10-05 22:10:36 +0530

    third commit

--- Car.java
+++ Car.java
@@ -2,12 +2,14 @@ public class Car {
     int wheels = 4;
     String color = "black";
     int average = 35;
+    int cc = 1000;
     public static void main(String[] args) {
         Car c = new Car();
         System.out.println("hii please start the car");
         System.out.println("color : "+c.color);
         System.out.println("No of wheels :"+ c.wheels);
         System.out.println("average :"+c.average);
+        System.out.println("cc : "+c.cc);
         c.start(true);
     }
     public void start(boolean b){
